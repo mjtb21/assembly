a52e8d2c8c3be2b2e55d679e4d7853ed5adb80fa
Mojtaba <mjtb21@gmail.com>
2025-07-29 23:12:34 +0330

    add help

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,8 @@ export default function Header() {
             <div className="title">Assembly: Endgame</div>
             <div className="info">
                 Guess the word in under 8 attempts to keep the programming world safe from Assembly!
+                <br/>
+                You can get help from <a href="https://ss-media.org/en/Word-8-letters/">here</a>
             </div>
         </div>
     )
